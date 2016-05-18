4253ec46b065b3469870089d160ae8719220d672
Nawid Jamali <nawid.jamali@iit.it>
2016-05-18 12:00:51 +0200

    up tolerence increased

--- src/hand.cpp
+++ src/hand.cpp
@@ -629,7 +629,7 @@ void Hand::waitMotionDone(const double period, const double timeout){
 }
 
 void Hand::relaxTolerence(){
-    _armCartesianCtrl->setInTargetTol(10/100);
+    _armCartesianCtrl->setInTargetTol(20/100);
 }
 
 void Hand::strictTolerence(){
